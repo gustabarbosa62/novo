b365e48d43680ba6cfecd3b4d9b2ce535e88af65
gustabarbosa62 <141523136+gustabarbosa62@users.noreply.github.com>
2023-08-05 16:21:44 -0400

    Create py.py
    fg

---
+++ py.py
@@ -0,0 +1,22 @@
+class ContaCorrente:
+  def __init__(self):
+    self.cpf = cpf
+    self.nome = nome
+    self.agencia = agencia
+    self.conta = conta
+    self.saldo = saldo
+
+  def consultar_saldo(self):
+    print(self.saldo)
+
+  def depositar(self):
+    self.deposito += deposito
+
+conta_gustavo = ContaCorrente()
+conta_gustavo.nome('Gustavo Barbosa Luiz')
+conta_gustavo.cpf(88499524575)
+conta_gustavo.agencia(006565)
+conta_gustavo.conta(8843)
+pritn(conta_gustavo)
+
+  
